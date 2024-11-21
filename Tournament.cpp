93e3a35b79f05fec7c74f4b0887880876c7d8944
DaveAber <51935200+DaveAber@users.noreply.github.com>
2024-11-21 18:38:42 +0000

    Add a Team class

--- Tournament.cpp
+++ Tournament.cpp
@@ -1,5 +1,16 @@
 #include "Tournament.h"
 
+
+void Team::dumpPlayers()
+{
+    cout << "Team with " << members.size() << " players:" << endl;
+    for (MythicalCreature& members: members)
+    {
+        cout << "\t" << members.display() << endl;
+    }
+}
+
+
 void Tournament::dumpPlayers()
 {
     cout << "Tournament play, " << players.size() << " players:" << endl;
--- Tournament.h
+++ Tournament.h
@@ -4,14 +4,29 @@
 #include "MythicalCreature.h"
 #include "RPS_Game.h"
 
+class Team
+{
+public:
+    Team() {};
+    ~Team() {};
+    vector<MythicalCreature> members;
+
+    // Add a player from the pool, to this particular team
+    int addPlayer(MythicalCreature& player) { members.push_back(player); return members.size(); }
+
+    void dumpPlayers();
+};
+
 class Tournament
 {
     vector<MythicalCreature> players;
+    vector<Team> teams;
 
 public:
     Tournament() {};
     ~Tournament() {};
 
+    // Players for the entire tournament.  Teams to be created from player pool, once all players added
     int addPlayer(MythicalCreature& player) { players.push_back(player); return players.size(); }
 
     void dumpPlayers();
